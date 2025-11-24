46575b773780bb29506a440282a7cb3ed0f3d042
rapsz30 <rafihendryansyah26@gmail.com>
2025-11-24 12:35:46 +0700

    Initial Commit

---
+++ Assets/Script/Fox.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using UnityEngine;
+
+public class Fox : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Animator anim;
+
+    float dirX;
+    float moveSpeed = 5f;
+
+    int healthPoints = 3;
+    bool isHurting;
+    bool facingRight = true;
+    Vector3 localScale;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        localScale = transform.localScale;
+    }
+
+    void Update()
+    {
+        // Input movement
+        dirX = Input.GetAxisRaw("Horizontal") * moveSpeed;
+
+        // Jump
+        if (Input.GetButtonDown("Jump") && rb.velocity.y == 0)
+            rb.AddForce(Vector2.up * 600f);
+
+        // Running (speed only, no anim)
+        if (Input.GetKey(KeyCode.LeftShift))
+            moveSpeed = 10f;
+        else
+            moveSpeed = 5f;
+
+        SetAnimationState();
+    }
+
+    void FixedUpdate()
+    {
+        if (!isHurting)
+            rb.velocity = new Vector2(dirX, rb.velocity.y);
+    }
+
+    void LateUpdate()
+    {
+        CheckWhereToFace();
+    }
+
+    void SetAnimationState()
+    {
+        // WALKING = ada input horizontal + sedang tidak lompat/jatuh
+        anim.SetBool("isWalking", dirX != 0 && rb.velocity.y == 0);
+
+        // Jumping
+        anim.SetBool("isJumping", rb.velocity.y > 0);
+
+        // Falling
+        anim.SetBool("isFalling", rb.velocity.y < 0);
+    }
+
+    void CheckWhereToFace()
+    {
+        if (dirX > 0) facingRight = true;
+        else if (dirX < 0) facingRight = false;
+
+        if ((facingRight && localScale.x < 0) || (!facingRight && localScale.x > 0))
+            localScale.x *= -1;
+
+        transform.localScale = localScale;
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.name.Equals("Frog"))
+            healthPoints-= 1;
+
+        if (col.gameObject.name.Equals("Frog") && healthPoints > 0)
+        {
+            anim.SetTrigger("isHurting");
+            StartCoroutine("Hurt");
+        }
+        else
+        {
+            dirX = 0;
+            isHurting = true;
+            anim.SetTrigger("isHurting");
+        }
+    }
+
+    IEnumerator Hurt()
+    {
+        isHurting = true;
+        rb.velocity = Vector2.zero;
+
+        if (facingRight)
+            rb.AddForce(new Vector2(-200f, 200f));
+        else
+            rb.AddForce(new Vector2(200f, 200f));
+
+        yield return new WaitForSeconds(0.5f);
+        isHurting = false;
+    }
+}
